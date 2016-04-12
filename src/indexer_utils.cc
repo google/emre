94c73f0301af4a9a6753378d6b49b59a21dc5e70
Frank Kuehnel <kuehnelf@google.com>
2016-04-12 15:23:19 -0700

    added missing license header

--- src/indexer_utils.cc
+++ src/indexer_utils.cc
@@ -1,10 +1,23 @@
+// Copyright 2012-2016 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // We export functions into R with lowercase and underscores.
 // The R API has corresponding camelcase functions.
 
-#include "contentads/analysis/caa/search_plus/regmh/emre/src/base/logging.h"
-#include "contentads/analysis/caa/search_plus/regmh/emre/src/base/stringprintf.h"
-#include "contentads/analysis/caa/search_plus/regmh/emre/src/indexers/bias_indexer.h"
-#include "contentads/analysis/caa/search_plus/regmh/emre/src/indexers/memory_indexer.h"
+#include "base/logging.h"
+#include "base/stringprintf.h"
+#include "indexers/bias_indexer.h"
+#include "indexers/memory_indexer.h"
 
 // Rcpp defines an ERROR macro that interferes with base/logging.h
 #include <Rcpp.h>
